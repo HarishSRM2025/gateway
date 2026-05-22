8ab702ef2a490c93d5f79608fb22e51586b0f135
HarishSRM2025 <harishsrm25@gmail.com>
2026-05-22 11:16:37 +0530

    latest

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config(); 
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const express = require("express");
 const { verify_token } = require("./middleware/verify_token");
 const cors = require("cors");
